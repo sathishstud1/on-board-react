a5cea2f32710959e986f340759b595abe0890927
sathishstud1 <57126621+sathishstud1@users.noreply.github.com>
2020-07-05 18:10:53 -0400

    AWS update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import 'react-app-polyfill/ie9'; // For IE 9-11 support
-import 'react-app-polyfill/stable';
+import "react-app-polyfill/ie9"; // For IE 9-11 support
+import "react-app-polyfill/stable";
 // import 'react-app-polyfill/ie11'; // For IE 11 support
-import './polyfill'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
+import "./polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL =
+  "http://ec2-35-166-58-245.us-west-2.compute.amazonaws.com:8080";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/sagas/verifyGoogleLogin.js
+++ src/sagas/verifyGoogleLogin.js
@@ -1,16 +1,22 @@
-import axios from 'axios';
-import {authenticated, VERIFY_GOOGLE_LOGIN} from "../store/authentication/acions";
-import {takeLatest, call, put} from 'redux-saga/effects';
+import axios from "axios";
+import {
+  authenticated,
+  VERIFY_GOOGLE_LOGIN,
+} from "../store/authentication/acions";
+import { takeLatest, call, put } from "redux-saga/effects";
 
 function verifyGoogleLogin(id_token) {
-  return axios.post('http://localhost:8080/verifyGoogleLogin', {id_token})
+  return axios.post(
+    "http://ec2-35-166-58-245.us-west-2.compute.amazonaws.com:8080/verifyGoogleLogin",
+    { id_token }
+  );
 }
 
 function* verifyGoogleLoginEffect(action) {
   try {
     const id_token = action.googleLoginResponse.tokenId;
     const history = action.history;
-    
+
     /*yield put(authenticated(true, action.googleLoginResponse.profileObj));
     history.push('/dashboard');*/
     const response = yield call(verifyGoogleLogin, id_token);
@@ -18,7 +24,7 @@ function* verifyGoogleLoginEffect(action) {
       yield put(authenticated(true, action.googleLoginResponse));
       history.push(action.path);
     } else {
-      yield put(authenticated(false,null));
+      yield put(authenticated(false, null));
     }
   } catch (e) {
     yield put(authenticated(false, null));
